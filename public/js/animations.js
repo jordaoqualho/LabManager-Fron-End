d5403c91b058467c76c8782c978c6a7c96fbe048
jordaoqualho <jordaoqualho@gmail.com>
2020-11-20 10:43:28 -0300

    ignore

--- public/js/animations.js
+++ public/js/animations.js
@@ -1,34 +1,32 @@
-$(window).on("load", function () {
-  $("#preloader .progress").delay(1000).fadeOut();
-  $("#preloader").delay(1000).fadeOut("slow");
-  $("body").delay(1000).css({
-    overflow: "visible",
-  });
+$(window).on("load", function() {
+    $("#preloader .progress").delay(1000).fadeOut();
+    $("#preloader").delay(1000).fadeOut("slow");
+    $("body").delay(1000).css({
+        overflow: "visible",
+    });
 });
 
-document.addEventListener("DOMContentLoaded", function () {
-  const menu = document.querySelector(".burger-menu");
-  const sidebar = document.querySelector(".menu-mobile");
-  const sidebarOption = document.querySelector(".menu-mobile");
-  let menuOpen = false;
+document.addEventListener("DOMContentLoaded", function() {
+    const menu = document.querySelector(".burger-menu");
+    const sidebar = document.querySelector(".menu-mobile");
+    const sidebarOption = document.querySelector(".menu-mobile");
+    let menuOpen = false;
     menu.addEventListener("click", () => {
-    if (!menuOpen) {
-      menu.classList.add("open");
-      sidebar.classList.add("ativo");
-      menuOpen = true;
-    } else {
-      menu.classList.remove("open");
-      sidebar.classList.remove("ativo");
-      menuOpen = false;
-    }
-  });
-  sidebarOption.addEventListener("click", () => {
-    if (menuOpen) {
-      menu.classList.remove("open");
-      sidebar.classList.remove("ativo");
-      menuOpen = false;
-    }
-  });  
+        if (!menuOpen) {
+            menu.classList.add("open");
+            sidebar.classList.add("ativo");
+            menuOpen = true;
+        } else {
+            menu.classList.remove("open");
+            sidebar.classList.remove("ativo");
+            menuOpen = false;
+        }
+    });
+    sidebarOption.addEventListener("click", () => {
+        if (menuOpen) {
+            menu.classList.remove("open");
+            sidebar.classList.remove("ativo");
+            menuOpen = false;
+        }
+    });
 });
-
-
